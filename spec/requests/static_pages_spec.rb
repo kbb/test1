f5f5c2f8ba8506be60105c3418c39e139dd774c9
yui <mailcontact.yui@gmail.com>
2014-06-30 07:17:10 +0000

    add a full_title heler

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,13 +11,15 @@ describe "Static pages" do
       expect(page).to have_content('Test1 App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}Home")
-      expect(page).to have_title("Home")
+      expect(page).to have_title("Test1 | ")
       # expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
     end
-
+    it "should NOT have a custome page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('Home')
+    end
   end
 
     describe "Help page" do
