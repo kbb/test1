3d2ab044e9191585534c98f06354883bbda52f3b
yui <mailcontact.yui@gmail.com>
2014-07-03 04:34:32 +0000

    finish chapter6 practice

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -100,10 +100,18 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
-end
 
+    describe "EMAIL with Mixed Case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
+      it "should be saved as all lower-case" do
+        @user.email = mixed_case_email
+        @user.save
+        expect(@user.reload.email).to eq mixed_case_email.downcase
+      end
 
+    end
 
+  end
 
 end
